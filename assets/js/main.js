a3159d59af9d8e05755ff7aef2aca02b48396717
jhenriqueb12 <henrique.barros.hb@outlook.com>
2022-03-07 12:35:47 -0300

    Finalização do design

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 
 let menu = true
 
+const headerUser = document.querySelector('.header-navbar__user')
+const mainContent = document.querySelector('.main-content')
 const sidebar = document.querySelector(".main-sidebar")
 const hamburguerMenu = document.querySelector(".header-menu__hamburguer")
 const hamburguerOne = document.querySelector(".header-menu__hamburguer--one")
@@ -9,14 +11,42 @@ const hamburguerTwo = document.querySelector(".header-menu__hamburguer--two")
 const hamburguerThree = document.querySelector(".header-menu__hamburguer--three")
 
 hamburguerMenu.addEventListener('click', () =>{
-  
+
+  headerUser.addEventListener('click', () => {
+    if(menu === false){
+      sidebar.classList.remove("active")
+      hamburguerTwo.classList.remove("toggle")
+      hamburguerOne.classList.remove("toggle")
+      hamburguerThree.classList.remove("toggle")
+      document.body.style.overflow = menu ? "hidden" : "initial"
+    }
+  })
+
+  mainContent.addEventListener('click', () => {
+    if(menu === false){
+      sidebar.classList.remove("active")
+      hamburguerTwo.classList.remove("toggle")
+      hamburguerOne.classList.remove("toggle")
+      hamburguerThree.classList.remove("toggle")
+      document.body.style.overflow = menu ? "hidden" : "initial"
+    }
+  })
+
+  document.body.style.overflow = menu ? "hidden" : "initial"
+
   sidebar.classList.toggle("active", menu)
+  
   hamburguerTwo.classList.toggle("toggle", menu)
   hamburguerOne.classList.toggle("toggle", menu)
-  hamburguerThree.classList.toggle("toggle", menu)  
-  
-  menu = !menu
+  hamburguerThree.classList.toggle("toggle", menu)
+
+  if(menu === true){
+    menu = false
+  }else {
+    menu = true
+  }
 })
+
 
 /* Main Content */
 
