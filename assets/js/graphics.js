b8ccc2212a7c7dafaddb14c31c8ff1f805d137dd
jhenriqueb12 <henrique.barros.hb@outlook.com>
2022-03-08 12:22:20 -0300

    INicializando a customização dos gráficos

--- assets/js/graphics.js
+++ assets/js/graphics.js
@@ -1,46 +1,53 @@
+const ctxOne = document.querySelector('.content-report__graphic--one')
 
-function drawChart() {
+const myChartOne = new Chart(ctxOne, {
+  type: 'line',
   
-  const graphicOne = document.querySelector('.content-report__graphic--one')
-
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Day');
-  data.addColumn('number', 'Guardians of the Galaxy');
-  data.addColumn('number', 'The Avengers');
-  data.addColumn('number', 'Transformers: Age of Extinction');
-
-  data.addRows([
-    [1,  37.8, 80.8, 41.8],
-    [2,  30.9, 69.5, 32.4],
-    [3,  25.4,   57, 25.7],
-    [4,  11.7, 18.8, 10.5],
-    [5,  11.9, 17.6, 10.4],
-    [6,   8.8, 13.6,  7.7],
-    [7,   7.6, 12.3,  9.6],
-    [8,  12.3, 29.2, 10.6],
-    [9,  16.9, 42.9, 14.8],
-    [10, 12.8, 30.9, 11.6],
-    [11,  5.3,  7.9,  4.7],
-    [12,  6.6,  8.4,  5.2],
-    [13,  4.8,  6.3,  3.6],
-    [14,  4.2,  6.2,  3.4]
-  ]);
-
-  var options = {
-    chart: {
-      title: 'Box Office Earnings in First Two Weeks of Opening',
-      subtitle: 'in millions of dollars (USD)'
+  data: {
+    labels: ['06/10', '06/10',  '06/10', '06/10', '06/10', '06/10'],
+    
+    datasets: [{
+      label: 'Estornado',
+      data: [50, 100, 200],
+      backgroundColor: '#425DC7',
+      borderColor: '#425DC7',
+      borderWidth: 2
     },
-    width: 900,
-    height: 500,
-    axes: {
-      x: {
-        0: {side: 'top'}
+    
+    {
+      label: 'Cancelado',
+      data: [30, 130, 230],
+      backgroundColor: '#F03460',
+      borderColor: '#F03460',
+      borderWidth: 2
+    },
+    
+    {
+      label: 'Não Pago',
+      data: [10, 110, 140],
+      backgroundColor: '#FFBE00',
+      borderColor: '#FFBE00',
+      borderWidth: 2
+    },
+
+    {
+      label: 'Pago',
+      data: [70, 180, 10],
+      backgroundColor: '#2EB042',
+      borderColor: '#2EB042',
+      borderWidth: 2
+    },
+  ]
+  },
+
+  options: {
+
+    responsive: true,
+
+    scales: {
+      y: {
+        beginAtZero: true
       }
     }
-  };
-
-  var chart = new google.charts.Line(document.getElementById('line_top_x'));
-
-  chart.draw(data, google.charts.Line.convertOptions(options));
-}
+  }
+})
